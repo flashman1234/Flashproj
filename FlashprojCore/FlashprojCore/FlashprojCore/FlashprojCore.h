e0ff0706926ebc34e567340e39b5e2643d3b5ded
Michal Thompson <flashman1234@gmail.com>
2021-06-01 11:35:18 +0200

    more core project stuff

---
+++ FlashprojCore/FlashprojCore/FlashprojCore/FlashprojCore.h
@@ -0,0 +1,18 @@
+//
+//  FlashprojCore.h
+//  FlashprojCore
+//
+//  Created by Michal Thompson on 01.06.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FlashprojCore.
+FOUNDATION_EXPORT double FlashprojCoreVersionNumber;
+
+//! Project version string for FlashprojCore.
+FOUNDATION_EXPORT const unsigned char FlashprojCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlashprojCore/PublicHeader.h>
+
+
